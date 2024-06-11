1dd77b4802f05d008043d09a0c3810203649f736
Andrew Ellen <18004410@uhi.ac.uk>
2024-06-11 20:13:31 +0100

    Added the code to test predictions with a trained model. It works

--- main.py
+++ main.py
@@ -32,11 +32,11 @@ def init_params():
   # Generates random values between 0 and 1
   W1 = np.random.randn(10, 784)
   # In tutorial he subtracts 0.5 from the below line. He also does the above line then says that breaks it.
-  b1 = np.random.randn(10, 1) - 0.5
+  b1 = np.random.randn(10, 1)
 
-  W2 = np.random.randn(10, 10) - 0.5
+  W2 = np.random.randn(10, 10)
 
-  b2 = np.random.randn(10, 1) - 0.5
+  b2 = np.random.randn(10, 1)
   
   return W1, b1, W2, b2
 
@@ -120,4 +120,24 @@ def gradient_descent(X, Y, iterations, alpha):
     return W1, b1, W2, b2
   
 
+# Function to make predictions
+def make_predictions(X, W1, b1, W2, b2):
+   _, _, _, A2 = forward_prop(W1, b1, W2, b2, X)
+   predictions = get_predictions(A2)
+   return predictions
+   
+# Function to test predictions
+def test_prediction(index, W1, b1, W2, b2):
+   current_image = X_train[:, index, None]
+   prediction = make_predictions(X_train[:, index, None], W1, b1, W2, b2)
+   label = Y_train[index]
+   print("Prediction: ", prediction)
+   print("Label ", label)
+
+   current_image = current_image.reshape((28,28)) * 255
+   plt.gray()
+   plt.imshow(current_image, interpolation="nearest")
+   plt.show()   
+   
 W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
+test_prediction(5, W1, b1, W2, b2)
