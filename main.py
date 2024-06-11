1246665ac08e3a916b172d6a488afa861856b18d
Andrew Ellen <18004410@uhi.ac.uk>
2024-06-11 19:49:37 +0100

    The neural net works but it won't train properly

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from IPython.display import display
 
 data = pd.read_csv("training_data/train.csv")
 
-display(data.head())
+#display(data.head())
 
 # Loading in the data into a numpy array. The data is the 28x28 pixel data for the numbers. 784 rows.
 data = np.array(data)
@@ -18,11 +18,13 @@ np.random.shuffle(data)
 data_dev = data[0:1000].T
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
+X_dev = X_dev / 255
 
 data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
-
+X_train = X_train / 255
+_,m_train = X_train.shape
 
 # Initialising the parameters
 def init_params():
@@ -32,8 +34,8 @@ def init_params():
   # In tutorial he subtracts 0.5 from the below line. He also does the above line then says that breaks it.
   b1 = np.random.randn(10, 1)
 
-  W2 = np.random.randn(10, 784)
-  # In tutorial he subtracts 0.5 from the below line. He also does the above line then says that breaks it.
+  W2 = np.random.randn(10, 10)
+
   b2 = np.random.randn(10, 1)
   
   return W1, b1, W2, b2
@@ -41,12 +43,12 @@ def init_params():
 
 # Function to handle rectified linear unit
 def ReLU(Z):
-  return np.maximum(0, Z)
+  return np.maximum(Z, 0)
 
 
 # Function to handle softmax
 def softmax(Z):
-  return np.exp(Z) / np.sum(np.exp(Z))
+  return (np.exp(Z) / np.sum(np.exp(Z)))
 
 
 # Function to handle forward propogation
@@ -54,7 +56,8 @@ def forward_prop(W1, b1, W2, b2, X):
   Z1 = W1.dot(X) + b1
   A1 = ReLU(Z1)
   Z2 = W2.dot(A1) + b2
-  A2 = softmax(A1)
+  A2 = softmax(Z2)
+  return Z1, A1, Z2, A2
 
 
 # Function to handle One hot encoding. This encodes categorical data into numerical ones.
@@ -72,17 +75,15 @@ def deriv_ReLU(Z):
 
 
 # Function to handle backwards propogation
-def back_prop(Z1, A1, Z2, A2, W2, X, Y):
-  # Z2 doesnt appear to be used in this function from the video so far so may be a mistake.
-  m = Y.size
-  one_hot_Y = one_hot(Y)
-  dZ2 = A2 - one_hot_Y
-  dW2 = 1 / m * dZ2.dot(A1.T)
-  db2 = 1 / m * np.sum(dZ2, 2)
-  dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
-  dW1 = 1 / m * dZ1.dot(X.T)
-  db1 = 1 / m * np.sum(dZ1, 2)
-  return dW1, db1, dW2, db2
+def back_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    db2 = 1 / m * np.sum(dZ2)
+    dZ1 = W2.T.dot(dZ2) * deriv_ReLU(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db1 = 1 / m * np.sum(dZ1)
+    return dW1, db1, dW2, db2
 
 
 # Function to update parameters
@@ -92,3 +93,31 @@ def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
   W2 = W2 - alpha * dW2
   b2 = b2 - alpha * db2
   return W1, b1, W2, b2
+
+
+# Function to handle getting the prediction value
+def get_predictions(A2):
+  return np.argmax(A2, 0)
+
+
+# Function to handle
+def get_accuracy(predictions, Y):
+  print(predictions, Y)
+  return np.sum(predictions == Y) / Y.size
+
+
+# Function to handle gradient descent
+def gradient_descent(X, Y, iterations, alpha):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_prop(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = back_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if i % 50 == 0:
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+  
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, 500, 0.1)
